6f90e8dc1fc85dfc976ce62497b130cad445c6ea
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-03-14 08:39:22 +0100

    feat: add a new module for all extra functionality
    split to remove in the future
    
    eslint plugin

--- packages/utilities/vite.config.ts
+++ packages/utilities/vite.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('vite').UserConfig} */
 import dts from 'vite-plugin-dts';
+import pkg from './package.json' assert { type: 'json' };
 import { defineConfig, mergeConfig } from 'vite';
 import { resolve } from 'path';
 import { sharedViteConfig } from '@lyra/config/vite.config.shared';
@@ -7,14 +8,18 @@ export default mergeConfig(
 	sharedViteConfig(__dirname),
 	defineConfig({
 		build: {
-			cssCodeSplit: true,
-			target: 'esnext',
 			lib: {
 				entry: resolve(__dirname, 'src/index.ts'),
-				fileName: 'utilities',
 				name: 'utilities',
 			},
+			rollupOptions: {
+				external: [
+					...Object.keys(pkg.dependencies), // don't bundle dependencies
+					/^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+				],
+			},
+			target: 'esnext', // transpile as little as possible
 		},
-		plugins: [dts()],
+		plugins: [dts({ rollupTypes: true })], // emit TS declaration files
 	})
 );
