409b831cb5f38f3aeb632250d4b82e39eec07164
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-03-14 12:57:50 +0100

    feat: replace lodash function range for native implementation

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,5 +1,6 @@
+import { range } from './range/range';
+// lodash functions
 import chunk from 'lodash/chunk';
-import range from 'lodash/range';
 import groupBy from 'lodash/groupBy';
 import sortBy from 'lodash/sortBy';
 import remove from 'lodash/remove';
